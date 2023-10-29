2aea0488d93094eb6026ae35192ac6c76c036c15
Carlos <cgumuciolabbe@gmail.com>
2023-10-29 18:52:16 +0800

    feat: nav menu animated

--- components/NewTop.tsx
+++ components/NewTop.tsx
@@ -1,12 +1,25 @@
-"use client"
-import { Avatar, Box, Button, Drawer, IconButton, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
-import React, { useState } from 'react'
-import LanguageSelector from './LanguageSelector'
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import ConstructionIcon from "@mui/icons-material/Construction";
-import ContactMailIcon from "@mui/icons-material/ContactMail";
-import Person4Icon from "@mui/icons-material/Person4";
-import MenuIcon from "@mui/icons-material/Menu";
+'use client';
+import {
+  Avatar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+import LanguageSelector from './LanguageSelector';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import Person4Icon from '@mui/icons-material/Person4';
+import MenuIcon from '@mui/icons-material/Menu';
 
 type Route = {
   label: string;
@@ -16,37 +29,33 @@ type Route = {
 
 const pages: Route[] = [
   {
-    label: "About Me",
+    label: 'About Me',
     icon: <Person4Icon />,
-    link: "/",
+    link: '/',
   },
   {
-    label: "Projects",
+    label: 'Projects',
     icon: <BusinessCenterIcon />,
-    link: "/projects",
+    link: '/projects',
   },
   {
-    label: "Playground",
+    label: 'Playground',
     icon: <ConstructionIcon />,
-    link: "/under-construction",
+    link: '/under-construction',
   },
   {
-    label: "Contact",
+    label: 'Contact',
     icon: <ContactMailIcon />,
-    link: "/contact",
+    link: '/contact',
   },
 ];
 
 const NewTop = () => {
-  const [menuHovered, setMenuHovered] = useState(false)
-  const [menuOpened, setMenuOpened] = useState(false)
-  
-   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(
-    null
-  );
+  const [menuHovered, setMenuHovered] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -63,155 +72,177 @@ const NewTop = () => {
     setAnchorElUser(null);
   };
   return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        m: '1rem',
+      }}
+    >
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          m: '1rem'
-        }}
-      >
-        <Box sx={menuOpened ? {
-          display: 'grid',
-          gridTemplateAreas: `
-          "name button"
-          "menubody menubody"
-        `,
-          gridTemplateRows: '10% 80%',
-        gridTemplateColumns: '50% 50%',
-          justifyItems: 'center',
-          marginLeft: { xs: '16px', md: '3vw' },
-          transition: 'width 0.8s, height 0.8s',
-          transitionTimingFunction: 'ease',
-          borderRadius: '33px',
-          width: { 'xs': '84vw', md: '10vw' },
-          height: { 'xs': '90vh', },
-          backgroundColor: '#3E030E',
-        } : {
-          display: 'grid',
-        gridTemplateAreas: `
-          "name button"
-        `,
-        gridTemplateRows: '10% 80%',
-          gridTemplateColumns: '50% 50%',
-          justifyItems: 'center',
-          backgroundColor: '#8AAA79',
-          color: "#8C7284",
-          borderRadius: '33px',
-          width: { 'xs': '148px', md: '10vw' },
-          height: { 'xs': '36px', },
-          pl: {xs: '10px'},
-          transitionDuration: '1.6s',
-          transitionTimingFunction: 'ease',
-        }}
-        >
-          {/* <Box
-            sx={
-              menuOpened? {
-              display: 'grid',
-              gridTemplateAreas: `
-                "name button"
-                "menuBody menuBody"
-                "menuBody menuBody"
-                `
-                ,
-                p: '2rem 1rem',
-                
-              } : {
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                gap: '1rem',
-                alignItems: 'center'
+        sx={
+          menuOpened
+            ? {
+                display: 'grid',
+                paddingTop: '10px',
+                gridTemplateAreas: `
+                  "name button"
+                  "menubody menubody"
+                `,
+                gridTemplateRows: '36px 90%',
+                gridTemplateColumns: '25% 25%',
+                justifyItems: 'center',
+                marginLeft: { xs: '16px', md: '3vw' },
+                transition: 'width 0.8s, height 0.8s, gridTemplateRows 0.8s, grid-template-rows 0.8s',
+                borderRadius: '33px',
+                width: { xs: '84vw', md: '10vw' },
+                height: { xs: '90vh' },
+                backgroundColor: '#3E030E',
               }
-            }
-          > */}
-            <Typography variant='h1' sx={{
+            : {
+                display: 'grid',
+                paddingTop: '10px',
+                gridTemplateAreas: `
+                    "name button  language"
+                  `,
+                gridTemplateRows: '36px 0%',
+                gridTemplateColumns: '25% 25% 25% 25%',
+                justifyItems: 'center',
+                backgroundColor: '#8AAA79',
+                color: '#8C7284',
+                borderRadius: '33px',
+                width: { xs: '160px', md: '10vw' },
+                height: { xs: '36px' },
+                transition: 'width 1.6s, height 0.8s, gridTemplateRows 0.8s, grid-template-rows 0.8s',
+                transitionTimingFunction: 'ease',
+                alignItems: 'start',
+                marginLeft: { xs: '16px', md: '3vw' },
+              }
+        }
+      >
+        <Typography
+          variant='h1'
+          sx={{
             fontSize: '1.25rem',
             fontWeight: 'bold',
             color: menuOpened ? 'white' : '#3E030E',
-            gridArea: 'name'
-          }}>
-            Carlos
-          </Typography>
-          <Button variant="outlined" onClick={() => setMenuOpened(!menuOpened)} onMouseEnter={() => setMenuHovered(!menuHovered)} style={
-            menuOpened ? {
-              backgroundColor: 'white',
-              transitionDuration: '0.8s',
-              transitionTimingFunction: 'ease',
-              color: '#3E030E',
-              gridArea: 'button',
-              borderColor: '#3E030E',
-              borderWidth: '1px'
-            } : {
-              transitionDuration: '0.8s',
-              transitionTimingFunction: 'ease-out',
-              backgroundColor: '#3E030E',
-              color: 'white',
-              gridArea: 'button',
-              borderColor: 'white',
-              borderWidth: '1px'
-              
-              }
+            gridArea: 'name',
+            pl: { xs: '2rem' },
+          }}
+        >
+          Carlos
+        </Typography>
+        <Button
+          variant='outlined'
+          onClick={() => setMenuOpened(!menuOpened)}
+          onMouseEnter={() => setMenuHovered(!menuHovered)}
+          sx={
+            menuOpened
+              ? {
+                  gridArea: 'button',
+                  backgroundColor: 'white',
+                  transitionDuration: '0.8s',
+                  transitionTimingFunction: 'ease',
+                  color: '#3E030E',
+                  gridColumn: '3/5',
+                  borderColor: '#3E030E',
+                  borderWidth: '1px',
+                  maxWidth: '20vw',
+                }
+              : {
+                  gridArea: 'button',
+                  transitionDuration: '0.8s',
+                  transitionTimingFunction: 'ease-out',
+                  color: '#3E030E',
+                  backgroundColor: 'white',
+                  gridColumn: '3/5',
+                  borderColor: 'white',
+                  borderWidth: '1px',
+                  width: '1rem',
+                  padding: { xs: '18px 8px' },
+                  maxWidth: '20vw',
+                  border: '1px white',
+                }
           }
           onMouseLeave={() => setMenuHovered(!menuHovered)}
-            sx={{
-              maxHeight: '20px',
-              maxWidth: '20vw'
-            }}
-          >
-            Menu
-            </Button>
-          <Box style={menuOpened ? {
-            visibility: 'visible',
-          gridArea: 'menuBody',
-          gridColumn: '1/3',
-            gridRow: '2/3'
-            // transition: 'visible 1s height 0s',
-            // transitionDuration: '0.8s',
-            //   transitionTimingFunction: 'ease',
-          } : {
-            visibility: 'hidden',
-            gridArea: 'menuBody',
-            gridColumn: '1/3',
-            gridRow: '2/3'
-            
-            //   transition: 'visible 1s height 0s',
-            // transitionDuration: '0.8s',
-            //   transitionTimingFunction: 'ease',
-          }}
-
-          >
-            <Typography variant='h6'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolores voluptas, pariatur laudantium culpa earum enim dolorum cupiditate ipsam et quibusdam rerum aperiam neque nesciunt eos ab est incidunt optio.
-            </Typography>
-          </Box>
-          {/* </Box> */}
+        >
+          Menu
+        </Button>
+        <Box
+          sx={
+            menuOpened
+              ? {
+                  // display: 'none',
+                  gridArea: 'language',
+                  gridRow: '2/2',
+                  opacity: 0,
+                  transition: 'width 0.8s, opacity 1s',
+                }
+              : {
+                  position: 'absolute',
+                  top: '1rem',
+                  right: '10%',
+                  opacity: 1,
+                  gridArea: 'language',
+                  gridRow: '1/1',
+                  gridColumn: '4/5',
+                  transition: 'width 0.8s, opacity 1s',
+                }
+          }
+        >
+          <LanguageSelector />
         </Box>
-            <LanguageSelector show={menuOpened} />
-            <Drawer
-              anchor='bottom'
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block" },
-              }}
-            >
-              <List>
-                {pages.map((route, index) => (
-                  <ListItem key={route.link} disablePadding>
-                    <Link href={route.link}>
-                      <ListItemButton>
-                        <ListItemIcon>{route.icon}</ListItemIcon>
-                        <ListItemText primary={route.label} />
-                      </ListItemButton>
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
-            </Drawer>
-          </Box>
-  )
-}
+        <Box
+          style={
+            menuOpened
+              ? {
+                  opacity: 1,
+                  gridArea: 'menuBody',
+                  gridColumn: '1/5',
+                  gridRow: '2/3',
+                  transition: 'opacity 1.6s',
+                }
+              : {
+                  opacity: 0,
+                  gridArea: 'menuBody',
+                  gridColumn: '1/5',
+                  gridRow: '2/3',
+                  height: 0,
+                }
+          }
+        >
+          <Typography variant='h6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolores voluptas, pariatur laudantium culpa
+            earum enim dolorum cupiditate ipsam et quibusdam rerum aperiam neque nesciunt eos ab est incidunt optio.
+          </Typography>
+        </Box>
+        {/* </Box> */}
+      </Box>
 
-export default NewTop
+      <Drawer
+        anchor='bottom'
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+        sx={{
+          display: { xs: 'block' },
+        }}
+      >
+        <List>
+          {pages.map((route, index) => (
+            <ListItem key={route.link} disablePadding>
+              <Link href={route.link}>
+                <ListItemButton>
+                  <ListItemIcon>{route.icon}</ListItemIcon>
+                  <ListItemText primary={route.label} />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default NewTop;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,31 @@
 // React & dependencies
-import { FC } from "react";
-import Image from "next/image";
+import { FC, useContext } from 'react';
+import Image from 'next/image';
 
 // Material Components
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from '@mui/material';
 
 // My components
-import useWindowDimensions from "../utils/windowHook";
-import translate from "../utils/translation";
+import useWindowDimensions from '../utils/windowHook';
+import translate from '../utils/translation';
+import { StateContext } from '../context/StateContext';
 // Queries & Mutations
 
 // Typescript
 interface Props {}
 const Home: FC<Props> = ({}) => {
+  const { language } = useContext(StateContext);
   return (
     <Grid container>
       <Grid
         item
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          mt: "5vh",
-          mx: "3vw",
-          height: "400px",
-          width: "337px",
+          display: 'flex',
+          justifyContent: 'center',
+          mt: '5vh',
+          mx: '3vw',
+          height: '400px',
+          width: '337px',
         }}
       >
         {/* Image left */}
@@ -34,18 +36,18 @@ const Home: FC<Props> = ({}) => {
           width={400}
           height={337}
           style={{
-            borderRadius: "10px",
+            borderRadius: '10px',
           }}
         />
       </Grid>
       <Grid
         item
         sx={{
-          px: "5vw",
+          px: '5vw',
         }}
       >
         {/* Description right */}
-        <Typography variant='subtitle1'>{translate(0, "en")}</Typography>
+        <Typography variant='subtitle1'>{translate(0, language)}</Typography>
         <Typography variant='h4'>Carlos Gumucio Labbé</Typography>
         <Typography variant='h6'>Full Stack Developer</Typography>
       </Grid>
